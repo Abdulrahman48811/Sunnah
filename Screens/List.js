2966f5199b0b52815c3d98571e5ff622ab6cf38e
Hussein Hussein <husseinwkc@gmail.com>
2022-12-26 10:21:20 -0500

    render flatlist for topics

--- Screens/List.js
+++ Screens/List.js
@@ -1,16 +1,63 @@
-import { StyleSheet, Text, View, ActivityIndicator, Button } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator, Button, FlatList, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+
+
 
 const List = () => {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([])
+
+
+    const getTitle = async () => {
+        try {
+            const response = await fetch("https://hadeethenc.com/api/v1/categories/list/?language=en");
+
+            const json = await response.json();
+
+            setData(json)
+
+
+
+
+        } catch (error) {
+        } finally {
+            setLoading(false);
+        }
+
+    }
+  
+        const Item = ({ title}) => (
+            <View style={styles.item}>
+                <Text style={styles.title} >{title}</Text>
+            </View>
+        );
+        
+        const renderItem = ({item}) => (
+            <Item  title={item.title} />
+        )
+
+    useEffect(() => {
+        getTitle();
+    }, []);
+
     return (
-        <View style={styles.container}>
+        
+            <ScrollView  style={styles.container}>
             <View style={styles.header}>
                 <Text style={{ fontSize: 24, right: 40, top: 20, fontStyle: 'italic', color: '#9496a1' }}> List of Hadiths</Text>
-                 
+
             </View>
 
-            <Text style={{fontSize:24, top:140, left: 20}}>Topics:</Text>
-        </View>
+            <Text style={{ fontSize: 24, top: 140, left: 20 }}>Topics:</Text>
+
+            <View>
+                <FlatList 
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={item => item.id} />
+            </View>
+            </ScrollView>
+        
     )
 }
 export default List;
@@ -28,6 +75,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#5d5f66',
         alignItems: 'center'
 
-    }
-
+    },
+    item: {
+        backgroundColor: '#a1a2ab',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        top:150
+      },
+      title: {
+        fontSize: 15,
+      }
 })
