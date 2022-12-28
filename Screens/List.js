33e3c61ef224f9f5a147a00c01943d78a03004ce
Hussein Hussein <husseinwkc@gmail.com>
2022-12-28 10:07:24 -0500

    flat list updated so that you can see the last rendered item + touch added so that navigation could be set up

--- Screens/List.js
+++ Screens/List.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, ActivityIndicator, Button, FlatList, ScrollView } from 'react-native';
 import React, { useState, useEffect } from 'react';
 
+import { TouchableOpacity } from 'react-native';
 
 
 const List = () => {
@@ -25,39 +26,60 @@ const List = () => {
         }
 
     }
-  
-        const Item = ({ title}) => (
-            <View style={styles.item}>
-                <Text style={styles.title} >{title}</Text>
-            </View>
-        );
+
+
+    const Item = ({ title, id }) => (
+    
+        <TouchableOpacity
+        style={styles.item}
+        onPress={() => {
+            console.log(id)
+        }}
+       
+         
+        >
         
-        const renderItem = ({item}) => (
-            <Item  title={item.title} />
-        )
+          
+            <Text style={styles.title} >{title}
+            
+            </Text>
+       
+            
+        </TouchableOpacity>
+        
+    );
+
+    const renderItem = ({ item }) => (
+        <Item title={item.title} id={item.id} />
+    )
 
     useEffect(() => {
         getTitle();
     }, []);
 
     return (
-        
-            <ScrollView  style={styles.container}>
+
+        <ScrollView style={styles.container}  >
             <View style={styles.header}>
                 <Text style={{ fontSize: 24, right: 40, top: 20, fontStyle: 'italic', color: '#9496a1' }}> List of Hadiths</Text>
 
             </View>
 
-            <Text style={{ fontSize: 24, top: 140, left: 20 }}>Topics:</Text>
+            <Text style={{ fontSize: 24, top: 140, left: 20 }} >Topics:</Text>
 
-            <View>
-                <FlatList 
-                data={data}
-                renderItem={renderItem}
-                keyExtractor={item => item.id} />
+            <View >
+                <FlatList
+                    data={data}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id} 
+                   style={{top:150}}
+                   contentContainerStyle={{paddingBottom:200}}
+                   
+                    />
             </View>
-            </ScrollView>
-        
+           
+        </ScrollView>
+
     )
 }
 export default List;
@@ -81,9 +103,9 @@ const styles = StyleSheet.create({
         padding: 20,
         marginVertical: 8,
         marginHorizontal: 16,
-        top:150
-      },
-      title: {
+        
+    },
+    title: {
         fontSize: 15,
-      }
+    }
 })
