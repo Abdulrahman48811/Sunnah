4d0694518a6ed348b7b10f0bfe858bad1266efcd
Hussein Hussein <husseinwkc@gmail.com>
2022-12-31 18:47:40 -0500

    added a route and styled background just need to start on rendering

--- Screens/HadithByCategory.js
+++ Screens/HadithByCategory.js
@@ -6,11 +6,70 @@ import { TouchableOpacity } from 'react-native';
 
 
 const HadithList = (id) => {
-return (
- <View>
-  
- </View>
-)
+    const HadithID = id.route.params.HadithID;
+    const title = id.route.params.category
+    const [data, setData] = useState([]);
+    const [Loading, setLoading] = useState();
+    const getHadithList = async () => {
+
+        try {
+            const response = await fetch(
+                `https://hadeethenc.com/api/v1/hadeeths/list/?language=en&category_id=${HadithID}&page=1&per_page=100`
+            );
+
+            const json = await response.json();
+
+            setData(json.data);
+            console.log(data[0].title )
+            
+        } catch (error) {
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getHadithList();
+    }, []);
+
+
+    
+    
+
+    return (
+
+
+        <View style={styles.container}>
+          <View style={styles.header}>
+        <Text
+          style={{
+            fontSize: 24,
+            right: 40,
+            top: 20,
+            fontStyle: "italic",
+            color: "#9496a1",
+          }}
+        >
+          {" "}
+          {title}
+        </Text>
+      </View>
+      <Text></Text>
+        </View>
+    )
 }
 
-export default HadithList
+export default HadithList;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#7d6c82",
+    }, 
+    header: {
+        width: 493,
+        height: 70,
+        top: 80,
+        backgroundColor: "#5d5f66",
+        alignItems: "center",
+      },
+});
