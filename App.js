cc91f34e57ca6772e6da0228f5bd2f028d8d6e44
Hussein Hussein <husseinwkc@gmail.com>
2022-12-30 21:21:14 -0500

    some routing but deleted for new page, gonna research and add

--- App.js
+++ App.js
@@ -1,65 +1,67 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './Screens/HomeScreen';
-import List from './Screens/List';
-import { Icon } from 'react-native-elements';
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./Screens/HomeScreen";
+import List from "./Screens/List";
+import { Icon } from "react-native-elements";
+import HadithList from "./Screens/HadithByCategory";
 
 export default function App() {
   const tab = createBottomTabNavigator();
 
+  const HadithList = ({navigation}) => {
+ 
+    return (
+  
+    <HadithList/>
+)
+  }
+
+
+
   const BottomTab = () => {
     return (
-
       <tab.Navigator
         screenOptions={{
-          tabBarStyle: { backgroundColor: 'black' },
+          tabBarStyle: { backgroundColor: "#e3dce3" },
           headerShown: false,
+        }}
+      >
+        <tab.Screen
+          name="Home"
+          options={{
+            tabBarLabel: "Home",
+            tabBarLabelStyle: { color: "black" },
+            tabBarIcon: () => <Icon name="home" color="black" size={40} />,
+          }}
+          component={HomeScreen}
+        />
+        <tab.Screen
+          name="List"
+          options={{
+            tabBarLabel: "List",
 
-
-
-        }} >
-        <tab.Screen name='Home' options={{
-          tabBarLabel: 'Home',
-          tabBarLabelStyle: { color: 'white' },
-          tabBarIcon: () => (
-            <Icon name='home' color='white' size={40} />
-          )
-        }} component={HomeScreen} />
-        <tab.Screen name='List'
-
-          options={
-            {
-              tabBarLabel: 'List',
-              
-              tabBarLabelStyle: { color: 'white' },
-              tabBarIcon: () => (
-                <Icon name='book' color='blue' size={40} />
-              )
-
-            }}
-          component={List} />
-        <tab.Screen name='Favorites'
-
-          options={
-            {
-              tabBarLabel: 'Favorites',
-              tabBarLabelStyle: { color: 'white' },
-              tabBarIcon: () => (
-                <Icon name='heart' type='font-awesome' color='red' size={40} />
-              )
-
-            }}
-          component={List} />
+            tabBarLabelStyle: { color: "black" },
+            tabBarIcon: () => <Icon name="book" color="black" size={40} />,
+          }}
+          component={List}
+        />
+        <tab.Screen
+          name="Favorites"
+          options={{
+            tabBarLabel: "Favorites",
+            tabBarLabelStyle: { color: "black" },
+            tabBarIcon: () => (
+              <Icon name="heart" type="font-awesome" color="black" size={40} />
+            ),
+          }}
+          component={List}
+        />
       </tab.Navigator>
-    )
-  }
+    );
+  };
   return (
     <NavigationContainer>
       <BottomTab />
     </NavigationContainer>
-
-
   );
 }
-
