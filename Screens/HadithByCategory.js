59f49462d64654f293365843f26fbfdeb3d6f289
Hussein Hussein <husseinwkc@gmail.com>
2022-12-30 21:18:54 -0500

    added page for when someone presses on a category

---
+++ Screens/HadithByCategory.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, ActivityIndicator, Button, FlatList, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+
+import { TouchableOpacity } from 'react-native';
+
+
+
+const HadithList = (id) => {
+return (
+ <View>
+  
+ </View>
+)
+}
+
+export default HadithList
