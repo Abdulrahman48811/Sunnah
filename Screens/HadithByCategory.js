dd9986110194257c6e4b450fac6ff1d7ce2594d7
Hussein Hussein <husseinwkc@gmail.com>
2023-01-10 17:23:43 -0500

    fixed styling and flatlist error

--- Screens/HadithByCategory.js
+++ Screens/HadithByCategory.js
@@ -1,75 +1,112 @@
-import { StyleSheet, Text, View, ActivityIndicator, Button, FlatList, ScrollView } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  Button,
+  FlatList,
+  ScrollView,
+} from "react-native";
 
-import { TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from "react";
 
-
-
+import { TouchableOpacity } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 const HadithList = (id) => {
-    const HadithID = id.route.params.HadithID;
-    const title = id.route.params.category
-    const [data, setData] = useState([]);
-    const [Loading, setLoading] = useState();
-    const getHadithList = async () => {
+  const HadithID = id.route.params.HadithID;
+  const title = id.route.params.category;
+  const navigation = id.route.params.navigate;
 
-        try {
-            const response = await fetch(
-                `https://hadeethenc.com/api/v1/hadeeths/list/?language=en&category_id=${HadithID}&page=1&per_page=100`
-            );
+  const [data, setData] = useState([]);
+  const [Loading, setLoading] = useState();
+  const getHadithList = async () => {
+    try {
+      const response = await fetch(
+        `https://hadeethenc.com/api/v1/hadeeths/list/?language=en&category_id=${HadithID}&page=1&per_page=100`
+      );
 
-            const json = await response.json();
+      const json = await response.json();
 
-            setData(json.data);
-            console.log(data[0].title )
-            
-        } catch (error) {
-        } finally {
-            setLoading(false);
+      setData(json.data);
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getHadithList();
+  }, []);
+
+  const Item = ({ title, id }) => (
+    <TouchableWithoutFeedback
+      style={styles.ahadith}
+      onPress={() => {
+        navigation.navigate("HadithInfo", { itemid: id });
+        
+      }}
+    >
+      <Text
+        style={{ fontSize: 12, alignSelf: "center", top: 10, marginBottom: 20 }}
+      >
+        {title}
+      </Text>
+    </TouchableWithoutFeedback>
+  );
+  const renderItem = ({ item }) => <Item title={item.title} id={item.id} />;
+  return (
+    <View style={styles.container}>
+      <FlatList
+        ListHeaderComponent={
+          <>
+            <View style={styles.header}>
+              <Text
+                style={{
+                  fontSize: 20,
+                  right: 40,
+                  top: 24,
+                  fontStyle: "italic",
+                  color: "#197784",
+                }}
+              >
+                {" "}
+                {title}
+              </Text>
+            </View>
+          </>
         }
-    };
-
-    useEffect(() => {
-        getHadithList();
-    }, []);
-
-
-    
-    
-
-    return (
-
-
-        <View style={styles.container}>
-          <View style={styles.header}>
-        <Text
-          style={{
-            fontSize: 24,
-            right: 40,
-            top: 20,
-            fontStyle: "italic",
-            color: "#9496a1",
-          }}
-        >
-          {" "}
-          {title}
-        </Text>
-      </View>
-      <Text></Text>
-        </View>
-    )
-}
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ paddingBottom: 200 }}
+        
+      />
+    </View>
+  );
+};
 
 export default HadithList;
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#7d6c82",
-    }, 
-    header: {
-        width: 493,
-        height: 70,
-        top: 80,
-        backgroundColor: "#5d5f66",
-        alignItems: "center",
-      },
+  container: {
+    flex: 1,
+    backgroundColor: "#197784",
+  },
+  header: {
+    width: 493,
+    height: 70,
+    top: 80,
+    backgroundColor: "#4B4874",
+    alignItems: "center",
+  },
+  ahadith: {
+    backgroundColor: "#227DA4",
+    width: 349,
+    height: 130,
+    borderRadius: 22,
+    top:150,
+    borderColor: "black",
+    borderWidth: 1,
+    alignSelf: "center",
+    marginVertical: 8,
+  },
 });
