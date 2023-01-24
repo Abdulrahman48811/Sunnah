97187f72ca2c020af7b591f0f9ef28262dd2e797
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2023-01-23 21:25:40 -0800

    prettier

--- Screens/List.js
+++ Screens/List.js
@@ -6,13 +6,10 @@ import {
   Button,
   FlatList,
   ScrollView,
-  
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import { Input } from "react-native-elements";
 import { TouchableOpacity } from "react-native";
-
-
 
 const List = ({ navigation }) => {
   const [isLoading, setLoading] = useState(true);
@@ -34,27 +31,22 @@ const List = ({ navigation }) => {
   };
 
   const Item = ({ title, id }) => (
-
-
-  
-  
     <TouchableOpacity
-      
       onPress={() => {
-        navigation.navigate('HadithByCategory', { HadithID: id, category: title, navigate: navigation })
-
-
+        navigation.navigate("HadithByCategory", {
+          HadithID: id,
+          category: title,
+          navigate: navigation,
+        });
       }}
     >
       <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
+        <Text style={styles.title}>{title}</Text>
       </View>
     </TouchableOpacity>
-   
   );
 
-  const renderItem = ({ item }) => 
-  <Item  title={item.title} id={item.id} />;
+  const renderItem = ({ item }) => <Item title={item.title} id={item.id} />;
 
   useEffect(() => {
     getTitle();
@@ -62,37 +54,37 @@ const List = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-    <FlatList
-      ListHeaderComponent={
-        <>
+      <FlatList
+        ListHeaderComponent={
+          <>
+            <View style={styles.header}>
+              <Text
+                style={{
+                  fontSize: 24,
+                  right: 40,
+                  top: 20,
+                  fontStyle: "italic",
+                  color: "#9496a1",
+                }}
+              >
+                {" "}
+                List of Hadiths
+              </Text>
+            </View>
 
-          <View style={styles.header}>
             <Text
-              style={{
-                fontSize: 24,
-                right: 40,
-                top: 20,
-                fontStyle: "italic",
-                color: "#9496a1",
-                
-              }}
+              style={{ fontSize: 24, top: 140, left: 20, marginBottom: 150 }}
             >
-              {" "}
-              List of Hadiths 
+              Topics:
             </Text>
-          </View>
-
-          <Text style={{ fontSize: 24, top: 140, left: 20 ,marginBottom:150}}>Topics:</Text>
-        </>
-      }
-      data={data}
-      renderItem={renderItem}
-      keyExtractor={(item) => item.id}
-      
-      contentContainerStyle={{ paddingBottom: 200 }}
-    />
-</View>
-
+          </>
+        }
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ paddingBottom: 200 }}
+      />
+    </View>
   );
 };
 export default List;
@@ -108,19 +100,14 @@ const styles = StyleSheet.create({
     top: 80,
     backgroundColor: "#5d5f66",
     alignItems: "center",
-    
   },
   item: {
     backgroundColor: "#a1a2ab",
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
-    borderRadius:59,
-    borderColor:"blue"
-
-    
-    
-    
+    borderRadius: 59,
+    borderColor: "blue",
   },
   title: {
     fontSize: 15,
