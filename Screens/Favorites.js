9a1538100f3586663efbb7902f76f3c7282b254b
Hussein Hussein <husseinwkc@gmail.com>
2023-02-02 22:50:43 -0500

    color + imported favorites List

--- Screens/Favorites.js
+++ Screens/Favorites.js
@@ -5,12 +5,31 @@ import {
   ActivityIndicator,
   FlatList,
 } from "react-native";
-import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from "react";
 
 const Favorites = () => {
+    const [is, isnt] = useState([])
+   const [HadithID, SetHadithID] = useState([])
+    const getData = async (value) => {
+        try {
+          await AsyncStorage.getItem("fav").then((favs) => {
+            favs = favs == null ? [] : JSON.parse(favs);
+            isnt(new Set(favs))
+          });
+        } catch (error) {
+          console.error();
+        }
+      };
+       const id = [...is] 
+    console.log(id)
+      useEffect(() => {
+
+        getData();
+      }, []);
  return(
     <View style={styles.container}>
-
+    
     </View>
  )
 };
@@ -20,6 +39,6 @@ export default Favorites;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#727cd4",
+    backgroundColor: "#206565",
   },
 });
